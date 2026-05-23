f64d15287e401f5e60be7b46abf4d37de7bf7c5c
diodemusic <diodemusicuk@gmail.com>
2026-05-23 23:19:42 +0100

    Add attack state and input configuration for player character

--- player/player.gd
+++ player/player.gd
@@ -3,12 +3,13 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const JAB_DURATION = 0.3
 
 @onready var camera := get_viewport().get_camera_3d()
 
-enum State {IDLE, MOVE, JUMP, FALL, LAND}
+enum State {IDLE, MOVE, JUMP, FALL, LAND, ATTACK}
 var state := State.IDLE
-
+var attack_timer := 0.0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -31,6 +32,10 @@ func _physics_process(delta: float) -> void:
 
 			if not is_on_floor():
 				state = State.FALL
+			
+			if Input.is_action_just_pressed("attack") and is_on_floor():
+				attack_timer = JAB_DURATION
+				state = State.ATTACK
 		State.IDLE:
 			velocity.x = 0
 			velocity.z = 0
@@ -44,6 +49,10 @@ func _physics_process(delta: float) -> void:
 			
 			if not is_on_floor():
 				state = State.FALL
+			
+			if Input.is_action_just_pressed("attack") and is_on_floor():
+				attack_timer = JAB_DURATION
+				state = State.ATTACK
 		State.JUMP:
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
@@ -59,6 +68,14 @@ func _physics_process(delta: float) -> void:
 			if direction:
 				state = State.MOVE
 			else:
+				state = State.IDLE
+		State.ATTACK:
+			velocity.x = 0
+			velocity.z = 0
+
+			attack_timer -= delta
+
+			if attack_timer <= 0:
 				state = State.IDLE
 
 	move_and_slide()
