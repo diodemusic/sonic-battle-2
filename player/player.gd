58acd303fc7f641466a119e29174a87033759169
diodemusic <diodemusicuk@gmail.com>
2026-05-23 21:59:13 +0100

    Refactor player movement logic to use state management for improved control flow

--- player/player.gd
+++ player/player.gd
@@ -6,25 +6,59 @@ const JUMP_VELOCITY = 4.5
 
 @onready var camera := get_viewport().get_camera_3d()
 
+enum State {IDLE, MOVE, JUMP, FALL, LAND}
+var state := State.IDLE
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction := (Vector3(input_dir.x, 0, input_dir.y)).rotated(Vector3.UP, camera.rotation.y).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	match state:
+		State.MOVE:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+
+			if not direction:
+				state = State.IDLE
+			
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+				state = State.JUMP
+
+			if not is_on_floor():
+				state = State.FALL
+		State.IDLE:
+			velocity.x = 0
+			velocity.z = 0
+
+			if direction:
+				state = State.MOVE
+			
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+				state = State.JUMP
+			
+			if not is_on_floor():
+				state = State.FALL
+		State.JUMP:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+
+			if velocity.y <= 0:
+				state = State.FALL
+		State.FALL:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+
+			if is_on_floor(): state = State.LAND
+		State.LAND:
+			if direction:
+				state = State.MOVE
+			else:
+				state = State.IDLE
 
 	move_and_slide()
